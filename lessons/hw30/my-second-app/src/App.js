e47cf3755e9ec814e0dbcfaf77d04f9c6e990b4d
Imotytskiy <Imotytskiy@gmail.com>
2023-07-23 22:14:46 +0200

    add button Show result

--- lessons/hw30/my-second-app/src/App.js
+++ lessons/hw30/my-second-app/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Button from "./components/Button/Button";
 import Smilelist from "./components/Smilelist/Smilelist";
 
 function App() {
   return (
     <div className="App">
       <Smilelist />
-      <Button>Show Result</Button>
     </div>
   );
 }
