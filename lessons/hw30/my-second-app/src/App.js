1a4b0a146fb5fed742ab32c28cb78b297e226d6d
Imotytskiy <Imotytskiy@gmail.com>
2023-07-23 11:44:26 +0200

    add

--- lessons/hw30/my-second-app/src/App.js
+++ lessons/hw30/my-second-app/src/App.js
@@ -1,10 +1,12 @@
-import UserComp from "./components/UserComp";
 import "./App.css";
+import { Button } from "./components/Button/";
+import { ProductList } from "./components/ProductList/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <UserComp />
+      <ProductList />
+      <Button>Button</Button>
     </div>
   );
 }
