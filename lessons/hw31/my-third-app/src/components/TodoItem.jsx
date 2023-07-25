b096e66e5e2ed919bcb7f0a3e19955d1c458a7dc
Imotytskiy <Imotytskiy@gmail.com>
2023-07-25 21:26:38 +0200

    add button

--- lessons/hw31/my-third-app/src/components/TodoItem.jsx
+++ lessons/hw31/my-third-app/src/components/TodoItem.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
+const styles = {
+  li: {
+    display: "flex",
+    justifyContent: "space between",
+    alignItems: "center",
+    padding: ".5rem 1rem",
+    border: "2px solid blue",
+    borderRadius: "5px",
+    marginBottom: ".5rem",
+  },
+  input: {
+    marginRight,
+  },
+};
+
 export default function TodoItem({ todo, index }) {
   return (
     <li>
-      <b>{index + 1}</b> {todo.title}
+      <span>
+        <input type="checkbox" style={styles.input} />
+        <b>{index + 1}. </b>
+        {todo.title}
+      </span>
+      <button className="rm">&times;</button>
     </li>
   );
 }
