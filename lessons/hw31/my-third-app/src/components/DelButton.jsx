020fc8a1e0ff7d95bde08f208261878fb7890447
Imotytskiy <Imotytskiy@gmail.com>
2023-07-26 02:06:16 +0200

    add

---
+++ lessons/hw31/my-third-app/src/components/DelButton.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// export default function DelButton({ todo, initialTodos, setInitialTodos }) {
+//   const handleDeleteTodo = () => {
+//     console.log(todo.id);
+//     const updatedData = initialTodos.filter((item) => item.id !== todo.id);
+//     setInitialTodos(updatedData);
+//   };
+
+//   return (
+//     <button onClick={handleDeleteTodo} className="rm">
+//       &times;
+//     </button>
+//   );
+// }
