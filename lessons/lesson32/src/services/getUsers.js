1f12ea959f59ce84bfd5c0af1fe587a192299c96
Imotytskiy <Imotytskiy@gmail.com>
2023-08-11 22:58:31 +0200

    add

--- lessons/lesson32/src/services/getUsers.js
+++ lessons/lesson32/src/services/getUsers.js
@@ -1,7 +1,7 @@
 export const getPostById = async ({ params }) => {
   try {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-    const data = awaitres.json();
+    const data = await res.json();
     return { users: data };
   } catch (error) {
     console.error(error);
