9f85cf2c691a02c72855212078416628de8e4817
Imotytskiy <Imotytskiy@gmail.com>
2023-08-13 20:29:00 +0200

    add third part

--- lessons/hw33v/src/routes/albums.jsx
+++ lessons/hw33v/src/routes/albums.jsx
@@ -5,22 +5,31 @@ import { useLoaderData } from "react-router-dom";
 export default function Albums() {
   const { albums } = useLoaderData();
   return (
-    <tbody>
-      {albums.map((album) => (
-        <tr key={album.id}>
-          <td>{album.title}</td>
-          <td>{album.userId}</td>
-          <td>{album.id}</td>
-          <td>
-            <NavLink className="new-contact" to="/">
-              PHOTOS
-            </NavLink>
-            <Link className="button-denied" to="/">
-              ПОВЕРНУТИСЯ
-            </Link>
-          </td>
+    <table>
+      <thead>
+        <tr>
+          <th>TITLE</th>
+          <th>USER</th>
+          <th>ALBUM</th>
         </tr>
-      ))}
-    </tbody>
+      </thead>
+      <tbody>
+        {albums.map((album) => (
+          <tr key={album.id}>
+            <td>{album.title}</td>
+            <td>{album.userId}</td>
+            <td>{album.id}</td>
+            <td>
+              <NavLink className="new-contact" to="/">
+                PHOTOS
+              </NavLink>
+              <Link className="button-denied" to="/">
+                ПОВЕРНУТИСЯ
+              </Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
--- lessons/hw33v/src/services/getPhotosById.js
+++ lessons/hw33v/src/services/getPhotosById.js
@@ -1,10 +1,13 @@
-export const getPostById = async ({ params }) => {
+export const getAlbums = async (params) => {
+  console.log(params);
+  console.log(params.params.albumId);
+  params.albumId;
   try {
     const res = await fetch(
-      `https://jsonplaceholder.typicode.com/photos/${params.getPostById}`
+      `https://jsonplaceholder.typicode.com/albums/${params.params.albumId}/photos`
     );
-    const data = awaitres.json();
-    return { post: data };
+    const data = await res.json();
+    return { albums: data };
   } catch (error) {
     console.error(error);
   }
