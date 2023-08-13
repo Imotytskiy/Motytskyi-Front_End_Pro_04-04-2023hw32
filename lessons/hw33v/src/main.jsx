751695a9b2ff64088182722b6dcfd277614bfbc9
Imotytskiy <Imotytskiy@gmail.com>
2023-08-13 23:18:10 +0200

    add

--- lessons/hw33v/src/main.jsx
+++ lessons/hw33v/src/main.jsx
@@ -8,24 +8,31 @@ import Photos from "./routes/photos";
 import { getUsers } from "./services/getUsers";
 import { getAlbums } from "./services/getAlbums";
 import { getPhotos } from "./services/getPhotos";
-import { ROUTES } from "./constans";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Users />,
-    loader: getUsers,
-  },
-  {
-    path: "/albums/:albumId",
-    element: <Albums />,
-    loader: getAlbums,
-  },
-  {
-    path: "/albums/:albumId/photos",
-    element: <Photos />,
-    loader: getPhotos,
-  },
-]);
+
+const basename = import.meta.env.DEV
+  ? "/"
+  : "/Motytskyi-Front_End_Pro_04-04-2023/";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Users />,
+      loader: getUsers,
+    },
+    {
+      path: "/albums/:albumId",
+      element: <Albums />,
+      loader: getAlbums,
+    },
+    {
+      path: "/albums/:albumId/photos",
+      element: <Photos />,
+      loader: getPhotos,
+    },
+  ],
+  { basename }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
